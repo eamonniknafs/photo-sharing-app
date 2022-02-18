05aba931cc13d97dd61d7f36cc20a91032754d7f
Eamon Niknafs <en@bu.edu>
2022-02-18 18:14:02 -0500

    updated album-photo relationship to be 1 to 1

--- skeleton/schema.sql
+++ skeleton/schema.sql
@@ -6,8 +6,9 @@ DROP TABLE IF EXISTS Tagged;
 DROP TABLE IF EXISTS Album_Contents;
 
 DROP TABLE IF EXISTS Comments CASCADE;
-DROP TABLE IF EXISTS Albums CASCADE;
+ALTER TABLE Albums DROP CONSTRAINT fk_album_cover;
 DROP TABLE IF EXISTS Pictures CASCADE;
+DROP TABLE IF EXISTS Albums CASCADE;
 DROP TABLE IF EXISTS Users CASCADE;
 DROP TABLE IF EXISTS Tags;
 
@@ -23,6 +24,17 @@ CREATE TABLE Users (
   password_hash varchar(255) NOT NULL
 );
 
+CREATE TABLE Albums
+(
+  album_id int4 AUTO_INCREMENT PRIMARY KEY NOT NULL,
+  name VARCHAR(255),
+  description VARCHAR(255),
+  date_created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  username VARCHAR(255) NOT NULL,
+  cover_photo_id int4,
+  CONSTRAINT fk_album_user FOREIGN KEY (username) REFERENCES Users(username)
+);
+
 CREATE TABLE Pictures
 (
   picture_id int4 AUTO_INCREMENT PRIMARY KEY NOT NULL,
@@ -32,8 +44,11 @@ CREATE TABLE Pictures
   date_created TIMESTAMP,
   last_edited TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   username varchar(255) NOT NULL,
-  CONSTRAINT fk_photo_user FOREIGN KEY (username) REFERENCES Users(username)
+  album_id int4,
+  CONSTRAINT fk_photo_user FOREIGN KEY (username) REFERENCES Users(username),
+  CONSTRAINT fk_photo_album FOREIGN KEY (album_id) REFERENCES Albums(album_id)
 );
+ALTER TABLE Albums ADD CONSTRAINT fk_album_cover FOREIGN KEY (cover_photo_id) REFERENCES Pictures(picture_id);
 
 CREATE TABLE Comments
 (
@@ -46,18 +61,6 @@ CREATE TABLE Comments
   username VARCHAR(255),
   CONSTRAINT FOREIGN KEY (picture_id) REFERENCES Pictures(picture_id),
   CONSTRAINT fk_comment_user FOREIGN KEY (username) REFERENCES Users(username)
-);
-
-CREATE TABLE Albums
-(
-  album_id int4 AUTO_INCREMENT PRIMARY KEY NOT NULL,
-  name VARCHAR(255),
-  description VARCHAR(255),
-  date_created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  username VARCHAR(255) NOT NULL,
-  cover_photo_id int4,
-  CONSTRAINT fk_album_user FOREIGN KEY (username) REFERENCES Users(username),
-  CONSTRAINT fk_album_cover FOREIGN KEY (cover_photo_id) REFERENCES Pictures(picture_id)
 );
 
 CREATE TABLE Tags
@@ -79,12 +82,4 @@ CREATE TABLE Tagged
   tag_name varchar(255) NOT NULL,
   CONSTRAINT fk_tagged_picture FOREIGN KEY (picture_id) REFERENCES Pictures(picture_id),
   CONSTRAINT fk_tagged_tag FOREIGN KEY (tag_name) REFERENCES Tags(tag_name)
-);
-
-CREATE TABLE Album_Contents
-(
-  album_id int4 NOT NULL,
-  picture_id int4 NOT NULL,
-  CONSTRAINT fk_album_contents_album FOREIGN KEY (album_id) REFERENCES Albums(album_id),
-  CONSTRAINT fk_album_contents_picture FOREIGN KEY (picture_id) REFERENCES Pictures(picture_id)
 );
